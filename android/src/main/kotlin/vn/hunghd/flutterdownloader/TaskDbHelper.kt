65719444068e28c1a1d74d462a44ed18181cdf18
ctenhank <ctenhank@gmail.com>
2025-03-17 15:44:44 +0900

    feat: Add Android Database Version Upgrade

--- android/src/main/kotlin/vn/hunghd/flutterdownloader/TaskDbHelper.kt
+++ android/src/main/kotlin/vn/hunghd/flutterdownloader/TaskDbHelper.kt
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper
 import android.provider.BaseColumns
 
 class TaskDbHelper private constructor(context: Context) :
-    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+        SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     override fun onCreate(db: SQLiteDatabase) {
         db.execSQL(SQL_CREATE_ENTRIES)
     }
@@ -15,6 +15,7 @@ class TaskDbHelper private constructor(context: Context) :
         update1to2(db, oldVersion)
         update2to3(db, oldVersion)
         update3to4(db, oldVersion)
+        update4to5(db, oldVersion)
     }
 
     private fun update1to2(db: SQLiteDatabase, oldVersion: Int) {
@@ -29,14 +30,31 @@ class TaskDbHelper private constructor(context: Context) :
         if (oldVersion > 2) {
             return
         }
-        db.execSQL("ALTER TABLE " + TaskEntry.TABLE_NAME + " ADD COLUMN " + TaskEntry.COLUMN_SAVE_IN_PUBLIC_STORAGE + " TINYINT DEFAULT 0")
+        db.execSQL(
+                "ALTER TABLE " +
+                        TaskEntry.TABLE_NAME +
+                        " ADD COLUMN " +
+                        TaskEntry.COLUMN_SAVE_IN_PUBLIC_STORAGE +
+                        " TINYINT DEFAULT 0"
+        )
     }
 
     private fun update3to4(db: SQLiteDatabase, oldVersion: Int) {
         if (oldVersion > 3) {
             return
         }
-        db.execSQL("ALTER TABLE ${TaskEntry.TABLE_NAME} ADD COLUMN ${TaskEntry.COLUMN_ALLOW_CELLULAR} TINYINT DEFAULT 1")
+        db.execSQL(
+                "ALTER TABLE ${TaskEntry.TABLE_NAME} ADD COLUMN ${TaskEntry.COLUMN_ALLOW_CELLULAR} TINYINT DEFAULT 1"
+        )
+    }
+
+    private fun update4to5(db: SQLiteDatabase, oldVersion: Int) {
+        if (oldVersion > 4) {
+            return
+        }
+        db.execSQL(
+                "ALTER TABLE ${TaskEntry.COLUMN_NAME_TIME_LAST_UPDATED} ADD COLUMN ${TaskEntry.COLUMN_NAME_TIME_LAST_UPDATED} TINYINT DEFAULT 0"
+        )
     }
 
     override fun onDowngrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
@@ -44,29 +62,46 @@ class TaskDbHelper private constructor(context: Context) :
     }
 
     companion object {
-        const val DATABASE_VERSION = 4
+        const val DATABASE_VERSION = 5
         const val DATABASE_NAME = "download_tasks.db"
         private var instance: TaskDbHelper? = null
-        private const val SQL_CREATE_ENTRIES = (
-            "CREATE TABLE " + TaskEntry.TABLE_NAME + " (" +
-                BaseColumns._ID + " INTEGER PRIMARY KEY," +
-                TaskEntry.COLUMN_NAME_TASK_ID + " VARCHAR(256), " +
-                TaskEntry.COLUMN_NAME_URL + " TEXT, " +
-                TaskEntry.COLUMN_NAME_STATUS + " INTEGER DEFAULT 0, " +
-                TaskEntry.COLUMN_NAME_PROGRESS + " INTEGER DEFAULT 0, " +
-                TaskEntry.COLUMN_NAME_FILE_NAME + " TEXT, " +
-                TaskEntry.COLUMN_NAME_SAVED_DIR + " TEXT, " +
-                TaskEntry.COLUMN_NAME_HEADERS + " TEXT, " +
-                TaskEntry.COLUMN_NAME_MIME_TYPE + " VARCHAR(128), " +
-                TaskEntry.COLUMN_NAME_RESUMABLE + " TINYINT DEFAULT 0, " +
-                TaskEntry.COLUMN_NAME_SHOW_NOTIFICATION + " TINYINT DEFAULT 0, " +
-                TaskEntry.COLUMN_NAME_OPEN_FILE_FROM_NOTIFICATION + " TINYINT DEFAULT 0, " +
-                TaskEntry.COLUMN_NAME_TIME_CREATED + " INTEGER DEFAULT 0, " +
-                    TaskEntry.COLUMN_NAME_TIME_LAST_UPDATED + " INTEGER DEFAULT 0, " +
-                TaskEntry.COLUMN_SAVE_IN_PUBLIC_STORAGE + " TINYINT DEFAULT 0, " +
-                TaskEntry.COLUMN_ALLOW_CELLULAR + " TINYINT DEFAULT 1" +
-                ")"
-            )
+        private const val SQL_CREATE_ENTRIES =
+                ("CREATE TABLE " +
+                        TaskEntry.TABLE_NAME +
+                        " (" +
+                        BaseColumns._ID +
+                        " INTEGER PRIMARY KEY," +
+                        TaskEntry.COLUMN_NAME_TASK_ID +
+                        " VARCHAR(256), " +
+                        TaskEntry.COLUMN_NAME_URL +
+                        " TEXT, " +
+                        TaskEntry.COLUMN_NAME_STATUS +
+                        " INTEGER DEFAULT 0, " +
+                        TaskEntry.COLUMN_NAME_PROGRESS +
+                        " INTEGER DEFAULT 0, " +
+                        TaskEntry.COLUMN_NAME_FILE_NAME +
+                        " TEXT, " +
+                        TaskEntry.COLUMN_NAME_SAVED_DIR +
+                        " TEXT, " +
+                        TaskEntry.COLUMN_NAME_HEADERS +
+                        " TEXT, " +
+                        TaskEntry.COLUMN_NAME_MIME_TYPE +
+                        " VARCHAR(128), " +
+                        TaskEntry.COLUMN_NAME_RESUMABLE +
+                        " TINYINT DEFAULT 0, " +
+                        TaskEntry.COLUMN_NAME_SHOW_NOTIFICATION +
+                        " TINYINT DEFAULT 0, " +
+                        TaskEntry.COLUMN_NAME_OPEN_FILE_FROM_NOTIFICATION +
+                        " TINYINT DEFAULT 0, " +
+                        TaskEntry.COLUMN_NAME_TIME_CREATED +
+                        " INTEGER DEFAULT 0, " +
+                        TaskEntry.COLUMN_NAME_TIME_LAST_UPDATED +
+                        " INTEGER DEFAULT 0, " +
+                        TaskEntry.COLUMN_SAVE_IN_PUBLIC_STORAGE +
+                        " TINYINT DEFAULT 0, " +
+                        TaskEntry.COLUMN_ALLOW_CELLULAR +
+                        " TINYINT DEFAULT 1" +
+                        ")")
         private const val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${TaskEntry.TABLE_NAME}"
 
         fun getInstance(ctx: Context?): TaskDbHelper {
